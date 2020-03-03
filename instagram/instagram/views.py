6fc18713f47350f8f7dce53dd9eef5c777b02815
rohid-hub <rohidwork303@gmail,com>
2020-03-03 21:49:06 +0600

    sticky side bar

--- instagram/instagram/views.py
+++ instagram/instagram/views.py
@@ -7,7 +7,7 @@ posts = [
         'discription': 'This is my first post!',
         'image': 'images/insta_pics/pic-1.jpg',
         'views': 301,
-        'profile_pic': 'images/profile.jpg',
+        'profile_pic': 'images/profile-3.jpg',
     },
     {
         'author': 'jasika_rr',
@@ -15,7 +15,7 @@ posts = [
         'discription': 'This is my first post also :-)!',
         'image': 'images/insta_pics/pic-2.jpg',
         'views': 458,
-        'profile_pic': 'images/profile.jpg',
+        'profile_pic': 'images/profile-2.jpg',
     },
     {
         'author': 'jon_301',
@@ -23,7 +23,7 @@ posts = [
         'discription': 'this is so cool!!!!',
         'image': 'images/insta_pics/pic-3.jpg',
         'views': 1003,
-        'profile_pic': 'images/profile.jpg'
+        'profile_pic': 'images/profile-4.jpg'
     },
 ]
 
